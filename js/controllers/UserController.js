bcc8e126335a733508fe97e97639d865488a9e31
Márcio Ramos Medeiros <80805761+MarcioRMed@users.noreply.github.com>
2022-02-11 00:13:06 -0300

    criando sistema login

---
+++ js/controllers/UserController.js
@@ -0,0 +1,63 @@
+class UserController{
+    constructor(){
+        console.log('UserController foi instanciada');
+
+        // this.addEventBtns();
+        // this.users = {}
+    }
+}
+    // addLine(user){
+
+
+    // }
+
+    // register(){
+        
+    //     let formEl = document.querySelector('.register');
+    //     let elements = formEl.elements;
+    //     let user;
+        
+    //     if(JSON.stringify(this.users) == JSON.stringify({})){
+
+    //         user = new User(0,elements.name.value,elements.photo.value,elements.email.value,elements.phone.value,elements.admin.value,elements.password.value);
+    //     }else{
+
+    //         let lastUser = Object.values({'a': 'teste', 'b': 'teste 2'})[Object.values({'a':'teste' , 'b': 'teste2'}).length-1];
+
+    //         user = new User(lastUser.getId()+1,elements.name.value,elements.photo.value,elements.email.value,elements.phone.value,elements.admin.value,elements.password.value);
+
+    //         console.log(user);
+
+
+    //     }
+
+    // }
+        
+
+    // }
+
+    // addEventListener(){
+
+    //     document.querySelector('.add').addEventListener('click', ()=> {
+    //         document.querySelector('.form-add').style.display = 'flex'
+    //     })
+
+    //     document.querySelectorAll('.close')[0].addEventListener('click', ()=>{
+    //         document.querySelector('.form-add').style.display = 'none'
+
+    //     })
+    //     document.querySelectorAll('.check')[0].addEventListener('click', ()=>{
+    //         this.register();
+            
+    //     })
+        
+    //     document.querySelectorAll('.close')[1].addEventListener('click', ()=>{
+    //         document.querySelector('.form-edit').style.display = 'none'
+
+    //     })
+
+    // }
+
+
+
+
